14bb6edc3647173d11c22e14e713143fa2b96b39
grab777 <grab777@gmail.com>
2026-02-07 23:30:43 +0100

    test

---
+++ frackwoche/server/main.py
@@ -0,0 +1,35 @@
+# a server that executes a command on the command line
+# use a popular python library to create a server that listens on a port and executes a command
+
+import socket
+import subprocess
+
+HOST = "localhost"
+PORT = 9990
+LED_ROWS = 64
+LED_COLS = 64
+FONT = "../fonts/5x8.bdf"
+command = f"./textdisplay --led-rows={LED_ROWS} --led-cols={LED_COLS} -f {FONT} -t "
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Server listening on port {PORT}...")
+    # if program is killed, close socket!
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            data = conn.recv(1024)
+            if not data:
+                break
+            message = data.decode("utf-8")
+            message = message.split("\r\n\r\n")[-1]  # get the body of the request
+            print(f"Sending message: {message}")
+            print(f"Executing command: {command} {message}")
+            try:
+                result = subprocess.check_output(
+                    command + message, shell=True, stderr=subprocess.STDOUT
+                )
+                conn.sendall(result)
+            except subprocess.CalledProcessError as e:
+                conn.sendall(e.output)
