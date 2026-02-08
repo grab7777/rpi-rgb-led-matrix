9119b9a43aec5744df520e36581537ebdbdf03a4
usrt0021 <usrt0021@students.zhaw.ch>
2026-02-08 15:19:05 +0100

    remove umlaut

--- frackwoche/server/main.py
+++ frackwoche/server/main.py
@@ -14,7 +14,7 @@ FONT = f"{BASE_FOLDER}/../fonts/5x8.bdf"
 command = f"sudo {BASE_FOLDER}/textdisplay --led-rows={LED_ROWS} --led-cols={LED_COLS} -f {FONT} -t"
 
 def sanitise_input(input_str):
-    return "".join([c for c in input_str if c.isalnum() or c.isspace() or c in ".,!?-ä�ÄÖÜ"]).strip()
+    return "".join([c for c in input_str if c.isalnum() or c.isspace() or c in ".,!?-"]).strip()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
